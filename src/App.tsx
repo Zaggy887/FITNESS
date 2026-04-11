d4a2b99506abdfc0be8128fe9eac224c490da09a
Zaggy887 <zsztrochlic@gmail.com>
2026-04-11 23:05:22 +1000

    Remove loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -8,39 +7,20 @@ import Testimonials from "./components/Testimonials";
 import Articles from "./components/Articles";
 import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
-import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoaded(true);
-    }, 1800);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <>
-      {!isLoaded && <LoadingScreen />}
-
-      <div
-        className={`min-h-screen transition-opacity duration-700 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        <Navbar />
-        <Hero />
-        <About />
-        <Process />
-        <Statistics />
-        <Testimonials />
-        <Articles />
-        <ContactForm />
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Process />
+      <Statistics />
+      <Testimonials />
+      <Articles />
+      <ContactForm />
+      <Footer />
+    </div>
   );
 }
 
