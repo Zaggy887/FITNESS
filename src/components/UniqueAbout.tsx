2ecd14d22081e7d70faf656cb218daca65941e1d
Zaggy887 <zsztrochlic@gmail.com>
2026-04-12 19:02:31 +1000

    Updated UniqueAbout.tsx

--- src/components/UniqueAbout.tsx
+++ src/components/UniqueAbout.tsx
@@ -40,7 +40,7 @@ const UniqueAbout = () => {
           WHAT <span className="text-[#7A725E]">WE GIVE YOU</span>
         </h2>
         <p className="fade-in text-white font-semibold text-sm uppercase tracking-widest mb-6">
-          "Give a man a fish and he eats for a day. Teach him to fish and he eats for a lifetime."
+          "If I give you a fish you'll eat for a day. If I teach you how to fish, you'll eat for a lifetime."
         </p>
 
         <p className="fade-in text-white font-bold text-lg md:text-xl leading-relaxed mb-6">
