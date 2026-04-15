4f74bd77c23a34b8481ef3418ac1119419d8386c
Zaggy887 <zsztrochlic@gmail.com>
2026-04-15 15:44:24 +1000

    Update hero gradient opacity

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <source src="/newhome.mp4" type="video/mp4" />
         </video>
 
-        <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-black/30 to-black/60" />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/15 to-black/35" />
       </div>
 
       {/* Text content section */}
